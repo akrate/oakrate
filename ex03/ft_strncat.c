5af5b2fa5ec17547eba338275a0c760a914ee691
Akrate Oussama <aoussama@e1r11p11.1337.ma>
2024-09-09 09:03:44 +0100

    hh

---
+++ ex03/ft_strncat.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aoussama <aoussama@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/07 18:22:23 by aoussama          #+#    #+#             */
+/*   Updated: 2024/09/08 16:44:27 by aoussama         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	unsigned int	i;
+	unsigned int	a;
+
+	a = 0;
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[a] != '\0' && a < nb)
+	{
+		dest[i] = src[a];
+		i++;
+		a++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
---
+++ ex04/ft_strstr.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aoussama <aoussama@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/08 11:49:40 by aoussama          #+#    #+#             */
+/*   Updated: 2024/09/08 13:00:25 by aoussama         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_len(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	if (*to_find == '\0')
+		return (str);
+	while (str[i] != '\0')
+	{
+		j = 0;
+		while (str[i + j] == to_find[j] && to_find[j] != '\0')
+		{
+			j++;
+		}
+		if (ft_len(to_find) == j)
+		{
+			return (&str[i]);
+		}
+		i++;
+	}
+	return (0);
+}
